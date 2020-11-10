cd5a895f8b9c4f0c3a9dd5dd4427baafb99f92dc
Nelson Rogers <nelsonlrrogers@gmail.com>
2020-11-10 13:28:22 +0100

    Implémentation

--- src/main/java/representation/Representant.java
+++ src/main/java/representation/Representant.java
@@ -1,5 +1,8 @@
 package representation;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Representant {
 
 	private final int numero;
@@ -7,11 +10,15 @@ public class Representant {
 	private final String prenom;
 	private String adresse;
 	private float salaireFixe;
+        private ZoneGeographique secteur;
+        private float caMensuel;
+        TreeMap <Integer, Float> CA = new TreeMap<>();
 
 	public Representant(int numero, String nom, String prenom, ZoneGeographique secteur) {
 		this.numero = numero;
 		this.nom = nom;
 		this.prenom = prenom;
+                this.secteur = secteur;
 	}
 
 	public int getNumero() {
@@ -43,13 +50,11 @@ public class Representant {
 	}
 
 	public ZoneGeographique getSecteur() {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		return secteur;
 	}
 
 	public void setSecteur(ZoneGeographique secteur) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+                this.secteur = secteur;
 	}
 
 	/**
@@ -65,8 +70,7 @@ public class Representant {
 		if (montant < 0) {
 			throw new IllegalArgumentException("Le montant doit être positif ou null");
 		}
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+                CA.put(mois, montant);
 	}
 
 	/**
@@ -76,8 +80,7 @@ public class Representant {
 	 * @return le salaire pour ce mois, tenant compte du salaire fixe, de l'indemnité repas, et du pourcentage sur CA
 	 */
 	public float salaireMensuel(int mois, float pourcentage) {
-		// TODO: Implémenter cette méthode
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		return salaireFixe + secteur.getIndemniteRepas() + pourcentage * CA.get(mois);
 	}
 
 	@Override
